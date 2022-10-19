3cc83245a88f20b76b88e92296a5b79a3a1c97f6
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2022-10-19 14:05:21 +0100

    Now the App shows the records of the DB
    Added the PHP code needed to show the records

---
+++ functions.php
@@ -0,0 +1,56 @@
+<?php
+    function connectToDB(){
+        $HOST = "localhost";
+        $DATABASE = "proyecto";
+        $USER = "usuario";
+        $PASSWORD = "1234";
+        $DSN = "mysql:host=$HOST;dbname=$DATABASE";
+        try{
+            return new PDO($DSN, $USER, $PASSWORD);
+        }catch(Exception $e){
+            die("Error: " . $e -> getMessage());
+        }
+    }
+    
+    function readDB(){
+        return connectToDB() -> query('SELECT * from productos');
+    }
+
+    function showRecords(){   
+        $records = readDB(); 
+        while($object = $records -> fetch(PDO::FETCH_OBJ))
+            echo "<tr>
+                    <td>
+                        <!-- Button Details -->
+                        <form method='POST' action='details.php'>
+                            <input name='action' type='hidden' value='edit'>
+                            <input name='id' type='hidden' value='" . $object -> id . "'>
+                            <input class='btn btn-info' type='submit' value='Detalles'>
+                        </form> 
+                    </td>
+                    <td>" . $object -> familia . "</td>
+                    <td>" . $object -> nombre . "</td>
+                    <td></td>
+                    <td>
+                        <div style='display: flex'>
+                            <!-- Button Edit -->
+                            <form method='POST' action='form.php'>
+                                <input name='action' type='hidden' value='edit'>
+                                <input name='id' type='hidden' value='" . $object -> id . "'>
+                                <input name='name' type='hidden' value='" . $object -> nombre . "'>
+                                <input name='initials' type='hidden' value='" . $object -> nombre_corto . "'>
+                                <input name='description' type='hidden' value='" . $object -> descripcion . "'>
+                                <input name='family' type='hidden' value='" . $object -> pvp . "'>
+                                <input class='btn btn-warning' type='submit' value='Actualizar'>
+                            </form> 
+                            <!-- Button Delete -->
+                            <form method='POST' action='index.php' style='margin-left: 10px'>
+                                <input name='action' type='hidden' value='delete'>
+                                <input name='id' type='hidden' value='" . $object -> id . "'>
+                                <input class='btn btn-danger' type='submit' value='Borrar'>
+                            </form>   
+                        </div>
+                    </td>
+                </tr>";
+    }    
+?>
--- index.php
+++ index.php
@@ -6,33 +6,18 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <?php include 'functions.php' ?>
     </head>
-    <body>
+    <body class="text-center">
         <div class="container">
             <!-- Header -->
+            <br><br>
             <div class="row">
                 <div class="col-12">
-                    <h1 class="text-center">Gestión de Productos</h1>
+                    <h1>Gestión de Productos</h1>
                 </div>
             </div>
-            <!-- Table -->
-            <div class="row">
-                <div class="col-12">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">Detalles</th>
-                                <th scope="col">Nombre</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php readDB(); ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+            <br>
             <!-- Button Insert -->
-            <div class="row">
+            <div class="row" style="margin-bottom: 15px;">
                 <div class="col-12">
                     <form method="POST" action="form.php">
                         <input name="action" type="hidden" value="insert">
@@ -40,6 +25,25 @@
                             <button class="btn btn-success" type="submit">Insertar</button>
                         </div>
                     </form>    
+                </div>
+            </div>
+            <!-- Table -->
+            <div class="row">
+                <div class="col-12">
+                    <table class="table table-striped table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col">Detalles</th>
+                                <th scope="col">Familia</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col"></th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php showRecords(); ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
